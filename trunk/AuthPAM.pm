91b82d7f9407aa6264a9b2604b522257b265ec76
tim.bunce <tim.bunce@71b5b796-e64d-0410-bf10-25ba38a42c9b>
2008-05-23 14:48:35 +0000

    First round of rough edits to move to Apache2::AuthPAM.

--- trunk/AuthPAM.pm
+++ trunk/AuthPAM.pm
@@ -1,38 +1,23 @@
-package Apache::AuthPAM;
-#
-# h2xs -AX -n Apache::AuthPAM
-#
+package Apache2::AuthPAM;
+
+=head1 NAME
+
+Apache::AuthPAM - Authenticate apache request using PAM services
+
+=cut
+
 use 5.006;
 use strict;
 use warnings;
-use Apache::Constants qw/:common/;
-use Apache::Log;
+
+use Apache2::Const qw/:common/;
+use Apache2::Log;
+
 use Authen::PAM qw/pam_start pam_end pam_authenticate pam_acct_mgmt PAM_SUCCESS/;
-
-require Exporter;
-
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Apache::AuthPAM ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
 
 our $VERSION = '0.01';
 
-our $MODNAME = 'Apache::AuthPAM';
+our $MODNAME = __PACKAGE__;
 
 #
 # I use this global to pass user info to the conversation function
@@ -108,8 +93,10 @@ sub handler {
   # Authenticated
   pam_end($pamh, 0);
   $log->info("$MODNAME: <$username> authenticated by $service", $r->uri);
+
   return OK;
 }
+
 
 #
 # Conversation Function
@@ -127,11 +114,6 @@ sub my_conv_func {
 
 1;
 __END__
-# Below is stub documentation for your module. You better edit it!
-
-=head1 NAME
-
-Apache::AuthPAM - Authenticate apache request using PAM services
 
 =head1 SYNOPSIS
 
@@ -139,7 +121,7 @@ Apache::AuthPAM - Authenticate apache request using PAM services
   <Directory /var/www/https/secured-area/>
      AuthType Basic
      AuthName "your server account"
-     PerlAuthHandler Apache::AuthPAM
+     PerlAuthHandler Apache2::AuthPAM
      PerlSetVar PAMservice check_user
      require valid-user
   </Directory>
@@ -161,32 +143,35 @@ You can select the PAM service setting the perl var PAMservice
 You can select different PAM services for different directories
 or locations in your web server filesystem space.
 
-Apache::AuthPAM works as follows:
+Apache2::AuthPAM works as follows:
 
 First, it calls C<pam_start> with the selected service.
 Second, it calls C<pam_authenticate> with the browser/apache supplied username and password.
 Later, it calls C<pam_acct_mgmt>.
 And finally it calls C<pam_end>.
-If any of the PAM functions fail, Apache::AuthPAM logs an info level message and returns C<AUTH_REQUIRED>.
-If all PAM functions are succesfull, Apache::AuthPAM logs an info level message and returns C<OK>.
+If any of the PAM functions fail, Apache2::AuthPAM logs an info level message and returns C<AUTH_REQUIRED>.
+If all PAM functions are succesfull, Apache2::AuthPAM logs an info level message and returns C<OK>.
 
-If you are going to use your system password database, you
-B<MUST> also use B<mod_ssl>.
+Remember that if you don't use https (SSL) then your username and password is
+transmitted on the network in plain text with each request. So if you are going
+to use your system password database, you B<MUST> also use B<mod_ssl> or you
+accounts will be easily compromised.
 
 =head1 BUGS
 
 I'am using a global symbol.
 
-Apache::AuthPAM is running as the same user mod_perl is running
+Apache2::AuthPAM is running as the same user mod_perl is running
 (on RedHat Linux it is apache). It is running without privileges.
 
 =head1 AUTHOR
 
-H�ctor Daniel Cort�s Gonz�lez E<lt>hdcg@cie.unam.mxE<gt>
+Tim Bunce L<http://www.tim.bunce.name> based on work by Héctor Daniel Cortés González E<lt>hdcg@cie.unam.mxE<gt>
 
 =head1 CREDITS
 
-Apache::AuthPAM is a direct adaptation of Demetrios E. Paneras'
+Apache2::AuthPAM is a direct adaptation of Héctor Daniel Cortés González's
+Apache::AuthPAM which was itself a direct adaptation of Demetrios E. Paneras'
 E<lt>dep@media.mit.eduE<gt> Apache::AuthenNISplus. 
 Authen::PAM is written by Nikolay Pelov E<lt>nikip@iname.comE<gt>.
 The sample PAM application check_user.c was contribuited by Shane Watts 
